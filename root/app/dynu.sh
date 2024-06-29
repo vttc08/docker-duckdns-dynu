3b0189d8132215d76af986b859c26583962fe118
Kevin Hu <hukevin69@gmail.com>
2024-06-29 16:48:34 -0700

    updated dynu ip update

--- root/app/dynu.sh
+++ root/app/dynu.sh
@@ -1,58 +1,32 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-echo "Hello from dynu."
-# if [[ "${LOG_FILE,,}" = "true" ]]; then
-#     DUCK_LOG="/config/duck.log"
-#     touch "${DUCK_LOG}"
-#     touch /config/logrotate.status
-#     /usr/sbin/logrotate -s /config/logrotate.status /app/logrotate.conf
-# else
-#     DUCK_LOG="/dev/null"
-# fi
+if [[ "${LOG_FILE}" = "true" ]]; then
+    DYNU_LOG="/config/dynu.log"
+    touch "${DYNU_LOG}"
+    touch /config/logrotate.status
+    /usr/sbin/logrotate -s /config/logrotate.status /app/logrotate.conf
+else
+    DYNU_LOG="/dev/null"
+fi
 
-# {
-#     # Use cloudflare to autodetect IP if UPDATE_IP is set
-#     if [[ "${UPDATE_IP}" = "ipv4" ]]; then
-#         echo "Detecting IPv4 via CloudFlare"
-#         IPV4=$(dig +short ch txt whoami.cloudflare -4 @1.1.1.1 | sed 's/"//g')
-#         DRESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&verbose=true")
-#         RESPONSE=$(echo "${DRESPONSE}" | awk 'NR==1')
-#         IPCHANGE=$(echo "${DRESPONSE}" | awk 'NR==4')
-#     elif [[ "${UPDATE_IP}" = "ipv6" ]]; then
-#         echo "Detecting IPv6 via CloudFlare"
-#         IPV6=$(dig +short ch txt whoami.cloudflare -6 @2606:4700:4700::1111 | sed 's/"//g')
-#         DRESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ipv6=${IPV6}&verbose=true")
-#         RESPONSE=$(echo "${DRESPONSE}" | awk 'NR==1')
-#         IPCHANGE=$(echo "${DRESPONSE}" | awk 'NR==4')
-#     elif [[ "${UPDATE_IP}" = "both" ]]; then
-#         echo "Detecting IPv4 and IPv6 via CloudFlare"
-#         IPV4=$(dig +short ch txt whoami.cloudflare -4 @1.1.1.1 | sed 's/"//g')
-#         IPV6=$(dig +short ch txt whoami.cloudflare -6 @2606:4700:4700::1111 | sed 's/"//g')
-#         DRESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4}&ipv6=${IPV6}&verbose=true")
-#         RESPONSE=$(echo "${DRESPONSE}" | awk 'NR==1')
-#         IPCHANGE=$(echo "${DRESPONSE}" | awk 'NR==4')
-#     else
-#     # Use DuckDns to autodetect IPv4 (default behaviour)
-#         echo "Detecting IPv4 via DuckDNS"
-#         DRESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=&verbose=true")
-#         IPV4=$(echo "${DRESPONSE}" | awk 'NR==2')
-#         IPV6=$(echo "${DRESPONSE}" | awk 'NR==3')
-#         RESPONSE=$(echo "${DRESPONSE}" | awk 'NR==1')
-#         IPCHANGE=$(echo "${DRESPONSE}" | awk 'NR==4')
-#     fi
+# Make the request to Dynu, assuming the docker container has the same address as the address you want to change
+RES=$(curl -sS "http://api.dynu.com/nsid/update?myip=10.0.0.0&password=${DYNU_PASS}&hostname=${DYNU_HOST}")
+echo $RES
+case $RES in
+    "badauth")
+        RESULT="\e[31mauthentication error\e[0m."
+        ;;
+    "nochg")
+        RESULT="IP update was not needed."
+        ;;
+    "good"*)
+        IP=$(echo $RES | cut -d' ' -f2)
+        RESULT="\e[32myour IP address was changed to $IP\e[0m."
+        ;;
+    *)
+        echo "Unknown response: $RES"
+        ;;
+esac
 
-#     if [[ "${RESPONSE}" = "OK" ]] && [[ "${IPCHANGE}" = "UPDATED" ]]; then
-#         if [[ "${IPV4}" != "" ]] && [[ "${IPV6}" == "" ]]; then
-#             echo "Your IP was updated at $(date) to IPv4: ${IPV4}"
-#         elif [[ "${IPV4}" == "" ]] && [[ "${IPV6}" != "" ]]; then
-#             echo "Your IP was updated at $(date) to IPv6: ${IPV6}"
-#         else
-#             echo "Your IP was updated at $(date) to IPv4: ${IPV4} & IPv6 to: {$IPV6}" 
-#         fi
-#     elif [[ "${RESPONSE}" = "OK" ]] && [[ "${IPCHANGE}" = "NOCHANGE" ]]; then
-#         echo "DuckDNS request at $(date) successful. IP(s) unchanged."
-#     else
-#         echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${DRESPONSE}\n"
-#     fi
-# } | tee -a "${DUCK_LOG}"
+echo -e "Dynu request completed at $(date), $RESULT" | tee -a "${DYNU_LOG}"
